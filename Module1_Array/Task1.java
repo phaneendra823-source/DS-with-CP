52fe722e27b4fb64ad8e94f7444de75a8c286c1f
phaneendra823-source <phaneendra823@gmail.com>
2025-12-22 15:23:49 +0530

    Enhance Task1.java with additional array examples
    Added new array initialization and printing logic.

--- Module1_Array/Task1.java
+++ Module1_Array/Task1.java
@@ -1,7 +1,8 @@
 class Main {
     public static void main(String[] args) {
         
-   
+        
+        // static D & I
         int[] num = {1,2,3};
         for(int i=0;i<num.length;i++){
             System.out.println(num[i]);
@@ -12,7 +13,7 @@ class Main {
             System.out.println(names[i]);
         }
         
-       
+        // new
          int[] num1 = new int[] {4,5,6};
         for(int i=0;i<num1.length;i++){
             System.out.println(num1[i]);
@@ -22,5 +23,13 @@ class Main {
         for(int i=0;i<names1.length;i++){
             System.out.println(names1[i]);
         }
+        
+        int[] da = new int[10];
+        da[0]=100;
+        da[da.length-1]=900;
+        for(int i=0;i<da.length;i++){
+            System.out.println(da[i]);
+        }
+        
  }
 }
