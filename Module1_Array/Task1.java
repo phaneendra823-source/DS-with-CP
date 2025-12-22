92785e8a8abfcbeebf061d2bdf675f2ceca018e8
phaneendra823-source <phaneendra823@gmail.com>
2025-12-22 15:07:34 +0530

    Add Task1.java with array examples and printing

---
+++ Module1_Array/Task1.java
@@ -0,0 +1,27 @@
+class Main {
+    public static void main(String[] args) {
+        
+        
+        // static D & I
+        int[] num = {1,2,3};
+        for(int i=0;i<num.length;i++){
+            System.out.println(num[i]);
+        }
+        
+        String[] names = {"sai","raju","sony"};
+        for(int i=0;i<names.length;i++){
+            System.out.println(names[i]);
+        }
+        
+        // new
+         int[] num1 = new int[] {4,5,6};
+        for(int i=0;i<num1.length;i++){
+            System.out.println(num1[i]);
+        }
+        
+        String[] names1 = new String[] {"lakshmi","kavya","phani"};
+        for(int i=0;i<names1.length;i++){
+            System.out.println(names1[i]);
+        }
+ }
+}
